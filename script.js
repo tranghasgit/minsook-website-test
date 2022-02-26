69cd8e2e6471ff99b7718375512541599652fbec
tranghasgit <tranghasmail@gmail.com>
2022-02-26 11:39:00 +0100

    update map function

--- script.js
+++ script.js
@@ -31,30 +31,39 @@ $(document).ready(function(){
   // ADD MARKERS-----------------------------------------------
   var LeafIcon = L.Icon.extend({
       options: {
-          shadowUrl: 'icon/img-shadow.png',
-          // iconSize:     [328, 492], // size of the icon
-          // shadowSize:   [50, 64],
+          // shadowUrl: 'icon/img-shadow.png',
+          iconSize:     [200], // size of the icon
+          // shadowSize:   [200],
           // iconAnchor:   [22, 94],
           // shadowAnchor: [4, 62],
       }
   });
 
-  var cIcon = new LeafIcon({iconUrl: 'icon/img-1.png', iconAnchor:[0,0], popupAnchor:  [255, 20], className: 'cIcon projIcons'}),
-      mIcon = new LeafIcon({iconUrl: 'icon/img-2.png', iconAnchor:[450,0], popupAnchor:  [-193, 32], className: 'mIcon projIcons'}),
-      yIcon = new LeafIcon({iconUrl: 'icon/img-3.png', iconAnchor:[900,0], popupAnchor:  [-642, 20], className: 'yIcon projIcons'}),
-      kIcon = new LeafIcon({iconUrl: 'icon/img-4.png', iconAnchor:[1350,0], popupAnchor:  [-1095, 50], className: 'kIcon projIcons'}),
-      myIcon = new L.divIcon({iconAnchor:[-520, -5], iconSize:[418, 582], className: 'myIcon projIcons'});
+  var cIcon = new LeafIcon({iconUrl: 'imgs/work/project-0/images/img-1.jpg', iconAnchor:[0,0], popupAnchor:  [255, 20], className: 'cIcon projIcons'}),
+      mIcon = new LeafIcon({iconUrl: 'imgs/work/project-1/island-performance/images/img-2.jpg', iconAnchor:[0,0], popupAnchor:  [-193, 32], className: 'mIcon projIcons'}),
+      yIcon = new LeafIcon({iconUrl: 'imgs/work/project-2/images/img-2.jpg', iconAnchor:[0,0], popupAnchor:  [-642, 20], className: 'yIcon projIcons'}),
+      kIcon = new LeafIcon({iconUrl: 'imgs/work/project-3/images/img-14.jpg', iconAnchor:[0,0], popupAnchor:  [-1095, 50], className: 'kIcon projIcons'}),
+      xIcon = new LeafIcon({iconUrl: 'imgs/work/project-4/images/img-3.jpg', iconAnchor:[0,0], popupAnchor:  [-1095, 50], className: 'kIcon projIcons'}),
+      myIcon = new L.divIcon({iconAnchor:[-520, -5], iconSize:[135, 200], className: 'myIcon projIcons'});
 
-  var cMarker = L.marker([25, 0], {icon: cIcon}),
-      mMarker = L.marker([1000, -850], {icon: mIcon}),
-      yMarker = L.marker([-500, 0], {icon: yIcon}),
-      kMarker = L.marker([250, 380], {icon: kIcon}),
-      myMarker = L.marker([0, 450], {icon: myIcon});
+  var cMarker = L.marker([300, 0], {icon: cIcon}),
+      mMarker = L.marker([10, -300], {icon: mIcon}),
+      yMarker = L.marker([-80, -690], {icon: yIcon}),
+      kMarker = L.marker([400, -100], {icon: kIcon}),
+      xMarker = L.marker([200, -350], {icon: xIcon}),
+      myMarker = L.marker([0, 25], {icon: myIcon});
 
-  cMarker.addTo(map).bindPopup("1. Image Exhibition The Christmas Tree Island. 2022. The Hague.");
-  mMarker.addTo(map).bindPopup("2. Image Exhibition The Christmas Tree Island. 2022. The Hague.");
-  yMarker.addTo(map).bindPopup("3. Image Exhibition The Christmas Tree Island. 2022. The Hague.");
-  kMarker.addTo(map).bindPopup("4. Image Exhibition The Christmas Tree Island. 2022. The Hague.");
+  // cMarker.addTo(map).bindPopup("1. Image Exhibition The Christmas Tree Island. 2022. The Hague.");
+  // mMarker.addTo(map).bindPopup("2. Image Exhibition The Christmas Tree Island. 2022. The Hague.");
+  // yMarker.addTo(map).bindPopup("3. Image Exhibition The Christmas Tree Island. 2022. The Hague.");
+  // kMarker.addTo(map).bindPopup("4. Image Exhibition The Christmas Tree Island. 2022. The Hague.");
+  // myMarker.addTo(map);
+
+  cMarker.addTo(map);
+  mMarker.addTo(map);
+  yMarker.addTo(map);
+  kMarker.addTo(map);
+  xMarker.addTo(map);
   myMarker.addTo(map);
 
   // L.marker([25, 0], {icon: cIcon}).addTo(map).bindPopup("1. Image Exhibition The Christmas Tree Island. 2022. The Hague.");
@@ -68,10 +77,31 @@ $(document).ready(function(){
   var mMarkerll = mMarker.getLatLng();
   var yMarkerll = yMarker.getLatLng();
   var kMarkerll = kMarker.getLatLng();
+  // var xMarkerll = xMarker.getLatLng();
   var myMarkerll = myMarker.getLatLng();
 
   $('.myIcon').append("<div class='img-wrapper'><img src='imgs/xmas-1/img-5.jpg'/><div class='pls-scroll'>scroll down ↓</div></div>").append("<p>The Promise of Ruin(s) tackles the concept of ruin(s)—as both a material relic and an intangible construct of the mind—into their diverse specificity, giving space to relational proposals and overlapping temporalities. Each of the three upcoming chapters echoes the ruin in its multiplicity, from the reading of modernity, to the speculative discrepancy of decay and precariousness of turbo-capitalism. <br> <br> This program is set to unfold at The Balcony over the course of the upcoming year. Each chapter is articulated as a trio exhibition. The aim is to bind established artists, with mid-career and recently graduated practitioners from the field.</>");
 
+
+  $('.projIcons').each(function(){
+    var newdiv = $('<div>', {
+      // id: 'some-id',
+      class: 'icon-shadow',
+    });
+
+    var iconW = $(this).width();
+    var iconH = $(this).height();
+    var iconX = $(this).offset().left;
+    var iconY = $(this).offset().top;
+
+
+    $(this).after(newdiv);
+    $('.icon-shadow').css({
+      "width" : iconW,
+      "height" : iconH,
+    });
+    // console.log("width: " + iconW + ", height: " + iconH);
+  });
 
   // menu items
   $(".menu ul li:nth-child(2)").click(function(){
@@ -103,22 +133,34 @@ $(document).ready(function(){
         var lat = myMarkerll.lat;
         var lng = myMarkerll.lng;
         map.flyTo([lat,lng]);
-        $(".myIcon").css({
-          'box-shadow': '10px 10px 10px yellow',
-        });
+        // $(".myIcon").css({
+        //   'box-shadow': '10px 10px 10px yellow',
+        // });
   });
 
   $(".thisTxtbox").click(function(){
-    $(".text").addClass("bgColorHl")
+    // $(".text").addClass("bgColorHl")
   });
 
-  $(".projIcons").each(function(){
+  $(".projIcons, .navProj").each(function(){
     $(this).click(function(){
       $(".sideBar").animate({right: 0});
+      if ($(".subSideBar").offset().left < $(window).width()) {
+        $(".subSideBar").animate({right: "-30vw"});
+      } else {}
     });
   });
 
-  $(".closeBtn").click(function(){
-    $(".sideBar").animate({right: "-30vw"});
+  $(".navSubProj").each(function(){
+    $(this).click(function(){
+      $(".subSideBar").animate({right: 0});
+      // $(".sideBar").animate({right: "60vw"});
+    });
+  });
+
+  $(".closeBtn").each(function(){
+    $(this).click(function(){
+      $(this).parent().animate({right: "-30vw"});
+    });
   });
 });
